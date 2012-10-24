0a3c97718a32c1675edac821699af203ff9cc752
rinshoundoshogai <rinshoundoshogai@a4f8b3a5-e007-e211-b16c-525400821517>
2012-10-24 09:47:02 +0000

    山本 修正されていなかったものを修正
    git-svn-id: svn+ssh://rinshoundoshogai.sakura.ne.jp/home/rinshoundoshogai/svn/rinsho@15 a4f8b3a5-e007-e211-b16c-525400821517

--- system/applicant_for_this_lesson.php
+++ system/applicant_for_this_lesson.php
@@ -16,24 +16,11 @@ border:1px #aaa solid;
 <h1 style='text-align:center;'>申込可能講座</h1></header>
 
 <?php
-	include_once ('./server_config.php');
 	$applicated_lesson_id = $_POST["applicated_lesson_id"];
-	$data1 = "select * from test_applicant where applicated_event_id='".$applicated_lesson_id."'";
+
+	include_once("./functions.php");
 	
-	$link = mysql_connect($sql_server, $sql_user, $sql_pw);
-	if (!$link) {
-		die('failed' . mysql_error());
-	}
-	
-	mysql_query("SET NAMES utf8",$link);
-	
-	$selecteddb = mysql_select_db($sql_db, $link);
-	
-	$strsql = $data1;
-	$res = mysql_query($strsql, $link);
-	if(!$res){
-		print mysql_errno($link) . ": " . mysql_error($link);
-	}
+	connect_to_mysql("select * from test_applicant where applicated_event_id='".$applicated_lesson_id."'");
 	
 	$num_rows = mysql_num_rows($res);
 	
