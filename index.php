ebf703109a6bcbc4d38ded4fe77c7b88b08b5b41
rinshoundoshogai <rinshoundoshogai@a4f8b3a5-e007-e211-b16c-525400821517>
2012-11-06 18:34:20 +0000

    山本 リンクの貼り直し
    git-svn-id: svn+ssh://rinshoundoshogai.sakura.ne.jp/home/rinshoundoshogai/svn/rinsho@27 a4f8b3a5-e007-e211-b16c-525400821517

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require('./wp_log/wp-load.php');
 
 $paged = get_query_var( 'paged' );
-query_posts('cat=5&posts_per_page=4&paged='.$paged);
+query_posts('cat=5&posts_per_page=5&paged='.$paged);
 ?>
 
 
@@ -11,7 +11,7 @@ query_posts('cat=5&posts_per_page=4&paged='.$paged);
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="utf-8">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <title>臨床運動障害研究会</title>
 <link href="style.css" rel="stylesheet" type="text/css">
 </head>
@@ -21,15 +21,15 @@ query_posts('cat=5&posts_per_page=4&paged='.$paged);
     <img src="./img/header.png" alt="header">
     <ul id="nav">  
 		<li><a href="index.html">Home</a></li>  
-		<li><a href="./staff/staff.html">Staff</a></li>  
-		<li><a href="./event/event.html">Event</a></li>  
+		<li><a href="./staff/index.html">Staff</a></li>  
+		<li><a href="./event/index.html">Event</a></li>  
 		<li><a href="./log/">Log</a></li>  
-		<li><a href="./volunteer/volunteer.html">Volunteer</a></li>  
+		<li><a href="./volunteer/index.html">Volunteer</a></li>  
 	</ul> 
   </header>
   <div id="main">
     <img src="./img/top.jpg" alt="top" width="950">
-	お知らせ
+	おしらせ
 	<?php
 	if(have_posts()) : while ( have_posts() ) : the_post();
 	?>
@@ -45,10 +45,10 @@ query_posts('cat=5&posts_per_page=4&paged='.$paged);
   <footer>
  <table border="0" width="950px" cellpadding="0">
   <tr>
-    <td><a href="./event/event.html"><img src="./img/IMG_0350.jpg" width="210" height="90" alt="勉強会"></a></td>
-    <td><a href="./volunteer/volunteer.html"><img src="./img/v.png" width="210" height="90" alt="海外ボランティア"></a></td>
+    <td><a href="./event/index.html"><img src="./img/IMG_0350.jpg" width="210" height="90" alt="勉強会"></a></td>
+    <td><a href="./volunteer/index.html"><img src="./img/v.png" width="210" height="90" alt="海外ボランティア"></a></td>
     <td><a href="http://ftp.minasehp.jp/"><img src="./img/m.png" width="210" height="90" alt="水無瀬病院"></a></td>
-    <td><a href="./mail/mail.html"><img src="./img/t.png" width="210" height="90" alt="お問い合わせ"></a></td>
+    <td><a href="./mail/index.html"><img src="./img/t.png" width="210" height="90" alt="お問い合わせ"></a></td>
   </tr>
 </table>
 	<small>
