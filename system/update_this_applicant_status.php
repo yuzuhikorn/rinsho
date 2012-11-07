b7966f26f299a59accd44e7d8e9b71b538121552
rinshoundoshogai <rinshoundoshogai@a4f8b3a5-e007-e211-b16c-525400821517>
2012-11-07 13:18:18 +0000

    山本 入金時に確認した旨を伝えるメールを送信するようにした
    git-svn-id: svn+ssh://rinshoundoshogai.sakura.ne.jp/home/rinshoundoshogai/svn/rinsho@36 a4f8b3a5-e007-e211-b16c-525400821517

--- system/update_this_applicant_status.php
+++ system/update_this_applicant_status.php
@@ -10,6 +10,11 @@
 			$row = mysql_fetch_assoc($res);
 			$body=$row['名前']."様へ\nキャンセル致しました。".$applicant_id;
 			mail_to_applicant($row['メール'],"キャンセル完了",$body);
+		}else if($status==3){
+			connect_to_mysql("select 名前,メール from applicant where 希望者ID='".$applicant_id."'");
+			$row = mysql_fetch_assoc($res);
+			$body=$row['名前']."様へ\n入金を確認致しました。\nありがとうございます。\nお会いできるのを楽しみにしております。".$applicant_id;
+			mail_to_applicant($row['メール'],"入金確認",$body);
 		}
 		mysql_query( "commit", $link);
 		mysql_close($link);
