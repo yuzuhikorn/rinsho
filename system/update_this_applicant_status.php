c298c3b5076aec111be0f60b831c37b31a95389e
rinshoundoshogai <rinshoundoshogai@a4f8b3a5-e007-e211-b16c-525400821517>
2012-11-07 07:56:28 +0000

    山本 キャンセルした人にキャンセル完了のメールを送信するようにした
    git-svn-id: svn+ssh://rinshoundoshogai.sakura.ne.jp/home/rinshoundoshogai/svn/rinsho@33 a4f8b3a5-e007-e211-b16c-525400821517

--- system/update_this_applicant_status.php
+++ system/update_this_applicant_status.php
@@ -1,11 +1,18 @@
 <?php
 	include_once("./functions.php");
 	connect_to_mysql("update applicant set status='".$status."' where applicant_id='".$applicant_id."' and applicated_lesson_id='".$lesson_id."'");
-		
+	
+	
 	if(!$res){
 		mysql_query( "rollback", $link);
 		mysql_close($link);
 	}else{
+		if($status==1){
+			connect_to_mysql("select mail from applicant where applicant_id='".$applicant_id."'");
+			$row = mysql_fetch_assoc($res);
+			$body=$name."様へ\nキャンセル致しました。";
+			mail_to_applicant($row['mail'],"キャンセル完了",$body);
+		}
 		mysql_query( "commit", $link);
 		mysql_close($link);
 		$counter++;
