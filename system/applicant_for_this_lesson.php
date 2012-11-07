7c85abca5392854ec55d5d13c92c3cfcd32fa057
rinshoundoshogai <rinshoundoshogai@a4f8b3a5-e007-e211-b16c-525400821517>
2012-11-07 07:44:48 +0000

    山本 キャンセルのボタンが押された後に条件があっていればキャンセル待ちの人にメールを送信するようにした 「キャンセル」か「入金済み」のステータスの希望者の場合、入金の有無のチェックボックスとキャンセルのボタンを非表示にするようにした
    git-svn-id: svn+ssh://rinshoundoshogai.sakura.ne.jp/home/rinshoundoshogai/svn/rinsho@32 a4f8b3a5-e007-e211-b16c-525400821517

--- system/applicant_for_this_lesson.php
+++ system/applicant_for_this_lesson.php
@@ -22,37 +22,42 @@ border:1px #aaa solid;
 	connect_to_mysql("select * from applicant where applicated_lesson_id='".$applicated_lesson_id."'");
 	
 	$num_rows = mysql_num_rows($res);
-		print '<form action="check_deposit.php" method="POST">';
-		print '<input type="hidden" name="status" value=3>';
-				print '<input type="hidden" name="lesson_id" value="'.$applicated_lesson_id.'">';
+	print '<form action="check_deposit.php" method="POST">';
+	print '<input type="hidden" name="status" value=3>';
+	print '<input type="hidden" name="lesson_id" value="'.$applicated_lesson_id.'">';
 	if(@$num = mysql_num_fields($res)){
-
+		
 		for($num_rows; $num_rows>0; $num_rows--){
 			while(@$data = mysql_fetch_row($res)){
-		print "<table style='margin:auto; border:hidden;'>\n";
-		print "<tr bgcolor=\"lightblue\">";
-		for($i = 0; $i < $num; $i++){
-			print "<td>";
-			print mysql_field_name($res, $i);
-			print "</td>";
-		}
-		print "</tr>\n";
-		while(@$data = mysql_fetch_row($res)){
-			print "<tr>";
-			for($j = 0; $j < $num; $j++){
-				print "<td>";
-				print $data[$j];
-				print "</td>";
-			}
-			print "<td style='border:hidden'>";
-
-				print '入金あり<input type="checkbox" name="applicant_id[]" value="'.$data[0].'">';
-				print '<input type="button" onClick="cancel('.$data[0].')" value="キャンセル">';
-			print "</td>";
-			print "</tr>\n";
-			
-		}
-		print "</table>\n";
+				print "<table style='margin:auto; border:hidden;'>\n";
+				print "<tr bgcolor=\"lightblue\">";
+				for($i = 0; $i < $num; $i++){
+					print "<td>";
+					print mysql_field_name($res, $i);
+					print "</td>";
+				}
+				print "</tr>\n";
+				while(@$data = mysql_fetch_row($res)){
+					print "<tr>";
+					for($j = 0; $j < $num; $j++){
+						print "<td>";
+						print $data[$j];
+						print "</td>";
+					}
+					print "<td style='border:hidden'>";
+					if($data[7]==1 ||$data[7]==3){
+						print "<div style='visibility:hidden'>";
+					}else{
+						print "<div>";
+					}
+					print '入金あり<input type="checkbox" name="applicant_id[]" value="'.$data[0].'">';
+					print '<input type="button" onClick="cancel('.$data[0].')" value="キャンセル">';
+					print '</div>';
+					print "</td>";
+					print "</tr>\n";
+					
+				}
+				print "</table>\n";
 				print "<div style='text-align:center;'>\n";
 				
 				
@@ -63,8 +68,8 @@ border:1px #aaa solid;
 		print '<input type="submit" value="入金情報確定">';
 		print '</form>';
 		
-		print '<form action="cancel_this_lesson.php" method="POST" name="form1">';
-		print '<input type="hidden" name="status" value=1>';
+		print '<form action="change_applicant_status.php" method="POST" name="form1">';
+		print '<input type="hidden" name="status" value=3>';
 		print '<input type="hidden" name="lesson_id" value="'.$applicated_lesson_id.'">';
 		print '<input type="hidden" name="applicant_id">';
 		print '</form>';
---
+++ system/change_applicant_status.php
@@ -0,0 +1,12 @@
+<?php
+	@$applicant_id = $_POST['applicant_id']."<br>";
+	@$lesson_id = $_POST['lesson_id']."<br>";
+	@$status = $_POST['status']."<br>";
+	include("./update_this_applicant_status.php");
+	if(!$res){
+		header("Location: ./failed.html");
+	}else{
+	include("./check_cancel.php");
+		header("Location: ./compleate.html");
+	}
+?>
