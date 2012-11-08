459d9be3c52fe471fa18206181f99ea9404ea2a9
rinshoundoshogai <rinshoundoshogai@a4f8b3a5-e007-e211-b16c-525400821517>
2012-11-08 13:30:19 +0000

    渡辺　トップページデザイン調整　お知らせと最新記事の一覧を表示するように
    git-svn-id: svn+ssh://rinshoundoshogai.sakura.ne.jp/home/rinshoundoshogai/svn/rinsho@42 a4f8b3a5-e007-e211-b16c-525400821517

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 require('./wp_log/wp-load.php');
-
-$paged = get_query_var( 'paged' );
-query_posts('cat=5&posts_per_page=5&paged='.$paged);
 ?>
 
 <!DOCTYPE HTML>
@@ -29,27 +26,45 @@ query_posts('cat=5&posts_per_page=5&paged='.$paged);
 	<div class="notice">
 		<h2>おしらせ</h2>
 		<?php
-		if(have_posts()) : while ( have_posts() ) : the_post();
-		?>
+		//paged = get_query_var( 'paged' );
+		//query_posts('cat=5&posts_per_page=5&paged='.$paged);
+		//if(have_posts()) : while ( have_posts() ) : the_post();
+		$args = array( 'numberposts' => 5, 'order'=> 'ASC', 'orderby' => 'title', 'cat' => '5' );
+		$postslist = get_posts( $args );
+		foreach ($postslist as $post) :  setup_postdata($post); ?> 
 		<ul>
 			<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> -<span class="time"><?php the_time('Y年m月d日（D）'); ?></span></li>
 		</ul>
 		<?php
-		endwhile;
-		endif;
+		endforeach;
+		//endwhile;
+		//endif;
 		?>
 		<a href="http://rinshoundoshogai.sakura.ne.jp/log/?page_id=45">お知らせ一覧へ</a>
 	</div>
-  <footer>
- <table border="0" width="950px" cellpadding="0">
-  <tr>
-    <td class="banner">ドミニカレポート バナー</td>
-    <td><a href="http://ftp.minasehp.jp/"><img src="./img/m.png" width="210" height="90" alt="水無瀬病院"></a></td>
-    <td><a href="./mail/index.html"><img src="./img/t.png" width="210" height="90" alt="お問い合わせ"></a></td>
-  </tr>
+	<div class="new">
+		<h2>最新報告</h2>
+		<?php
+		$args = array( 'numberposts' => 5, 'order'=> 'DESC');
+		$postslist = get_posts( $args );
+		foreach ($postslist as $post) :  setup_postdata($post); ?> 
+		<ul>
+			<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> -<span class="time"><?php the_time('Y年m月d日（D）'); ?></span></li>
+		</ul>
+		<?php
+		endforeach;
+		?>
+
+	</div>
+<footer>
+<table border="0" width="950px" cellpadding="0">
+<tr>
+<td class="banner">ドミニカレポート バナー</td>
+<td><a href="http://ftp.minasehp.jp/"><img src="./img/m.png" width="210" height="90" alt="水無瀬病院"></a></td>
+<td><a href="./mail/index.html"><img src="./img/t.png" width="210" height="90" alt="お問い合わせ"></a></td>
+</tr>
 </table>
-	<small>
-    Copyright c 2012 miyazaki-seminar All Rights Reserved. </small>
-  </footer>
+<small>Copyright c 2012 miyazaki-seminar All Rights Reserved. </small>
+</footer>
 </div>
 </html>
